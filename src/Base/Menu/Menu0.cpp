bc3561ca3a936ba2fa74eb8579f84c9fdfe5f91a
unknown <stefan.gaina01@gmail.com>
2022-01-17 12:33:26 +0200

    edit Menu0.cpp

--- src/Base/Menu/Menu0.cpp
+++ src/Base/Menu/Menu0.cpp
@@ -1,19 +1,12 @@
 #include "Menu0.h"
 
-Menu0::Menu0(SDL_Renderer* renderer) : write(renderer), frame(renderer), icon(renderer)
+Menu0::Menu0(SDL_Renderer* renderer) : 
+	frame(renderer)
 {
 }
 
-void Menu0::draw(Receipt human, Receipt orc, Faction turn)
+void Menu0::remember(Uint8 actionsLeft, bool hasAttacked)
 {
-	frame.draw(turn, type, hasAttacked);
-	icon.draw(turn, type, hasAttacked);
-	write.draw(turn, type, human, orc, actionsLeft);
-}
-
-void Menu0::set(State type, Uint8 actionsLeft, bool hasAttacked, bool vampState)
-{
-	this->type = type;
 	this->actionsLeft = actionsLeft;
 	this->hasAttacked = hasAttacked;
 }
